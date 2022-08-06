fb0653e2346e526f0caa42f2cd246c83354e6f3e
Rafael Alexandre <rafael.alexandre.1996.ra@gmail.com>
2022-08-05 23:35:19 -0300

    Adicionando deposito

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,7 +1,13 @@
 public class ContaCorrente {
-    private Cliente cliente;
+  private Cliente cliente;
+  private double saldo;
 
     public ContaCorrente(Cliente cliente) {
-        this.cliente = cliente;
+      this.cliente = cliente;
+      this.saldo = 0.0;
+    }
+
+    public void depositar(double valor) {
+      this.saldo += valor;
     }
 }
